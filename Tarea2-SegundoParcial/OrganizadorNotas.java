980e465933c828563a8bccff2ac64652aec11e9b
IVANCCAMA <81888538+IVANCCAMA@users.noreply.github.com>
2021-05-23 23:12:28 -0400

    Add files via upload

---
+++ Tarea2-SegundoParcial/OrganizadorNotas.java
@@ -0,0 +1,70 @@
+
+public class OrganizadorNotas{
+    private int numEst;
+    private double [] notas;
+    private int aux;
+    private double promedioDeNotas;
+    private double promedioDeNotasMayores;
+    private int estReprobados;
+    private int estAprobados;
+    private double mayorNota;
+    public OrganizadorNotas(int num){
+        numEst = num;
+        notas = new double[num];
+        aux = 0;
+        promedioDeNotas = 0;
+    }
+    
+    public void AñadirNota(double nota){
+        if(aux<notas.length){
+            notas[aux] = nota;
+            aux++;
+        }
+    }
+    public double PromedioDeNot(){
+        double promedio = 0;
+        int total = 0;
+        for(int i = 0; i<notas.length; i++){
+            promedio = promedio + notas[i];
+            total++;
+        }
+        promedioDeNotas = promedio/total;
+        return promedioDeNotas;
+    }
+    public double PromedioDeNotMayores(){
+        double promedio = 0;
+        int total = 0;
+        for(int i = 0; i<notas.length; i++){
+            if(notas[i]>51){
+                promedio = promedio + notas[i];
+                total++;
+            }
+        }
+        promedioDeNotasMayores = promedio/total;
+        return promedioDeNotasMayores;
+    }
+    public int Reprobados(){
+        for(int i = 0; i<notas.length; i++){
+            if(notas[i]<51){
+                estReprobados++;
+            }
+        }
+        return estReprobados;
+    }
+    public int Aprobados(){
+        for(int i = 0; i<notas.length; i++){
+            if(notas[i]>=51){
+                estAprobados++;
+            }
+        }
+        return estAprobados;
+    }
+    public double MayorNota2(){
+        for(int i = 0; i<notas.length; i++){
+            if(notas[i] > mayorNota){
+                mayorNota = notas[i];
+            }    
+        }
+        return mayorNota;
+    }
+}
